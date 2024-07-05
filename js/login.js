c9ff5619653a4f1027220e60b2f0d10763d75d6f
sebas350 <profesebastian8@gmail.com>
2024-07-05 11:51:42 -0300

    validacion de usuarios

--- js/login.js
+++ js/login.js
@@ -7,50 +7,53 @@ function redireccionar() {
 const loginForm = document.querySelector('#login-form');
 loginForm.onsubmit = login;
 
-function login(event) {
-    event.preventDefault();
-    let usuario = document.getElementById("usuario");
-    let clave = document.getElementById("clave");
-    let error = false;
-    document.getElementById("validar_usuario").innerHTML = "&nbsp; ";
-    document.getElementById("validar_clave").innerHTML = "&nbsp; ";
+async function dataFetch(){
+    try{
+        const response = await fetch("http://pysebas.pythonanywhere.com/usuarios");
+        const users = await response.json();
+        return users;  
+    }catch(error){
+        document.body.textContent =`"Error de red:" ${error.message}`;
+    };
+};
 
-    if (usuario.value == "") {
-        document.getElementById("validar_usuario").innerHTML = "Debe completar el usuario";
-        error = true;
-        usuario.focus();
-    }
-
-
-    if (clave.value.length < 8) {
-        document.getElementById("validar_clave").innerHTML = "Debe completar el clave con 8 caracteres como mínimo";
-        error = true;
-        clave.focus();
-    } else {
-        // Validar que la contraseña tenga al menos una letra mayúscula
-        let tieneMayuscula = false;
-        for (let i = 0; i < clave.value.length; i++) {
-            if (clave.value[i] === clave.value[i].toUpperCase() && clave.value[i] !== clave.value[i].toLowerCase()) {
-                tieneMayuscula = true;
-                break;
-            }
-        }
-        if (!tieneMayuscula) {
-            document.getElementById("validar_clave").innerHTML = "La contraseña debe contener al menos una letra mayúscula";
-            error = true;
-            clave.focus();
-        }
-    }
-
-    if (!error) {
-        if(usuario.value == "admin" && clave.value == "Admin123"){
+async function validarUsuario(nombre,password){
+    const users = await dataFetch();
+    const user = users.find(user => user.nombre === nombre && user.password === password);
+    if(user){
+        if(user.rol === 'admin'){
             sessionStorage.setItem('admin',true);
             window.location.href = "admin.html";
         }else{
             localStorage.setItem('user',usuario.value);
-            redireccionar()    
+            redireccionar();    
         };
-        
+    }else{
+        document.getElementById("validar_usuario").innerHTML = "Usuario inexistente o datos mal ingresados. Reintente";
+    };
+};    
+
+async function login(event) {
+    event.preventDefault(); 
+    let usuario = document.getElementById("usuario");
+    let clave = document.getElementById("clave");
+    let error = false;
+    
+    if (usuario.value == "") {
+        document.getElementById("validar_usuario").innerHTML = "Debe completar el usuario";
+        error = true;
+        usuario.focus();
     };
 
+
+    if (clave.value == "") {
+        document.getElementById("validar_clave").innerHTML = "Debe ingresar la clave";
+        error = true;
+        clave.focus();
+    };
+    
+    if(!error){
+        validarUsuario(usuario.value,clave.value);
+    };
+      
 };
